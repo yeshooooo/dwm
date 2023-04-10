5bd4047cdaed49f252917bcc7d9e85e51418d2c0
yeshooo <81931672@qq.com>
2023-04-10 23:59:55 +0800

    Patch -> patches/dwm-alpha-6.4.diff

--- config.def.h
+++ config.def.h
@@ -13,6 +13,8 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag	    = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=28" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=28";
+static unsigned int baralpha        = 0xd0;
+static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
