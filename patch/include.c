5d6f26c5df080aeafc1990ef934749e8455e87ea
yeshooooo <81931672@qq.com>
2023-04-14 17:33:49 +0800

    Add files

---
+++ patch/include.c
@@ -0,0 +1,32 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_anybar.c"
+#include "bar_ewmhtags.c"
+
+/* Other patches */
+#include "cool_autostart.c"
+#include "fullscreen.c"
+#include "ipc.c"
+#ifdef VERSION
+#include "ipc/IPCClient.c"
+#include "ipc/yajl_dumps.c"
+#include "ipc/ipc.c"
+#include "ipc/util.c"
+#endif
+#include "pertag.c"
+#include "placemouse.c"
+#include "restartsig.c"
+#include "rotatestack.c"
+#include "scratchpad.c"
+#include "sticky.c"
+#include "vanitygaps.c"
+#include "xrdb.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_gapplessgrid.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,27 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_anybar.h"
+#include "bar_ewmhtags.h"
+
+/* Other patches */
+#include "cool_autostart.h"
+#include "fullscreen.h"
+#include "ipc.h"
+#include "ipc/ipc.h"
+#include "ipc/util.h"
+#include "pertag.h"
+#include "placemouse.h"
+#include "restartsig.h"
+#include "rotatestack.h"
+#include "scratchpad.h"
+#include "sticky.h"
+#include "vanitygaps.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_gapplessgrid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
